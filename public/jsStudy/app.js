95b78c2334c166afc4c895d46e92961c3d0db98f
Jiwon93 <ls_sjw93@naver.com>
2023-02-09 23:50:02 +0900

    20230209_js_study2

--- public/jsStudy/app.js
+++ public/jsStudy/app.js
@@ -186,6 +186,46 @@ const KrAge = calculateKrAge(age);
 console.log(krAge);
 */
 
+/*
 const age = prompt("How old are you?");
 
 console.log(age, parseInt(age));
+*/
+
+const age = parseInt(prompt("How old are you?"));
+
+console.log(inNaN(age));
+
+if(condition) {
+	/// condition === true
+} else {
+	/// condition === false
+}
+
+// true || true === true
+// false || true === true
+// true || false === true
+// false || false === false
+// true && true === true
+// false && true === false
+// true && false === false
+// false && false === false
+
+// = value 할당 === 맞는지 확인
+if (isNaN(age) || age < 0) {
+	console.log("Please write a real positive number");
+} else if(age < 18) {
+	console.log("You are too young.");
+} else if(age >= 18 && age <= 50) {
+	console.log("You can drink");
+} else if(age > 50 && age <= 80) {
+	console.log("You should exercise");
+} else if(age === 100) {
+	console.log("wow you are wise");
+} else if(age > 80) {
+	console.log("You can do whatever you want");
+} 
+
+if((a && b) || (c && d) || (x || w)) {
+	
+}
