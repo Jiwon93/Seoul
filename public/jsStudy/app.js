12332023ede3108ed1eb5677339b5d9e9183e2bc
Jiwon93 <ls_sjw93@naver.com>
2023-02-12 21:16:54 +0900

    20230212_study_js1

--- public/jsStudy/app.js
+++ public/jsStudy/app.js
@@ -260,4 +260,14 @@ function handleTitleClick() {
 	console.log("title was clicked!");
 	title.style.color = "blue";
 }
+
+function handleMouseEnter() {
+	title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave() {
+	title.innerText = "Mouse is gone!";
+}
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
