477a77ddb40e927b3ba1a5f75662b2c80bf47000
Jiwon93 <ls_sjw93@naver.com>
2023-02-23 00:08:11 +0900

    20230223_study_js

--- public/jsStudy/app.js
+++ public/jsStudy/app.js
@@ -334,3 +334,16 @@ function fcn_jpg2csv(){
 jpg_path.addEventListener("change", fcn_jpg2csv);
 */
     
+//const loginForm = document.getElementById("login-form");    
+//const loginForm = document.querySelector("#login-form");
+//const loginInput = loginForm.querySelector("input");
+//const loginButton = loginForm.querySelector("button");    
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+	//console.log(loginInput.value);
+	console.log("hello", loginInput.value);
+}
+
+loginButton.addEventListener("click", onLoginBtnClick); 
