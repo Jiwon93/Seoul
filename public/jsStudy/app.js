a9f6d617bb44fb72c82e25c61e4f6c6540e17acc
Jiwon93 <ls_sjw93@naver.com>
2023-02-26 18:14:17 +0900

    20230226_study_js

--- public/jsStudy/app.js
+++ public/jsStudy/app.js
@@ -343,7 +343,16 @@ const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
 	//console.log(loginInput.value);
-	console.log("hello", loginInput.value);
+	//console.log("hello", loginInput.value);
+	const username = loginInput.value;
+	console.log(username);
+	/*
+	if (username === "") {
+		alert("Please write your name");
+	} else if (username.length > 15) {
+		alert("Your name is too long.");
+	}
+	*/
 }
 
 loginButton.addEventListener("click", onLoginBtnClick); 
