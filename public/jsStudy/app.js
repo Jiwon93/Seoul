25d8ebe17c1a627b32537ffacf1270ceb11c5930
Jiwon93 <ls_sjw93@naver.com>
2023-03-05 23:17:01 +0900

    20230305_study_js

--- public/jsStudy/app.js
+++ public/jsStudy/app.js
@@ -386,11 +386,9 @@ function paintGreetings(username) {
 const savedUsername = localStorage.getItem("USERNAME_KEY");
 
 if(savedUsername === nell) {
-	//show the form
 	loginForm.classList.remove(HIDDEN_CLASSNAME);
 	loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-	// show the greetings
 	paintGreetings(savedUsername);
 }
 
